eb90c41212a1ba8e7a042be8aeb5197744c44693
Nathan Herrmann <nathan.herrmann@hotmail.de>
2020-12-19 11:32:08 +0100

    added logger; implemented scheduling

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
+	"os"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/nhe23/aq-dbsync/pkg/dataprocessor"
 
+	"github.com/jasonlvhit/gocron"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -22,6 +25,12 @@ const measurementsColName = "measurements"
 const citiesColName = "cities"
 const countriesColName = "countries"
 
+type dataProcessParams struct {
+	url          string
+	col          collection
+	callBackFunc func(url string, collection *mongo.Collection) (int, error)
+}
+
 type collections struct {
 	measurementCol collection
 	citiesCol      collection
@@ -29,32 +38,33 @@ type collections struct {
 }
 
 type collection struct {
-	name       string
-	collection *mongo.Collection
+	name string
+	col  *mongo.Collection
 }
 
-func initCollections(mongoURI string, dbName string) collections {
+var logger log.Logger
+
+func initCollections(mongoURI string, dbName string) (collections, error) {
+	var cols collections
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
-		log.Fatal(err)
+		return cols, err
 	}
 
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		log.Fatal(err)
+		return cols, err
 	}
 
 	db := client.Database(dbName)
-	var cols collections
-
 	cols.countriesCol.name = countriesColName
 	cols.measurementCol.name = measurementsColName
 	cols.citiesCol.name = citiesColName
 
-	cols.countriesCol.collection = db.Collection(cols.countriesCol.name)
-	cols.citiesCol.collection = db.Collection(cols.citiesCol.name)
-	cols.measurementCol.collection = db.Collection(cols.measurementCol.name)
-	indexName, err := cols.measurementCol.collection.Indexes().CreateOne(
+	cols.countriesCol.col = db.Collection(cols.countriesCol.name)
+	cols.citiesCol.col = db.Collection(cols.citiesCol.name)
+	cols.measurementCol.col = db.Collection(cols.measurementCol.name)
+	_, err = cols.measurementCol.col.Indexes().CreateOne(
 		context.Background(),
 		mongo.IndexModel{
 			Keys: bson.M{
@@ -64,10 +74,9 @@ func initCollections(mongoURI string, dbName string) collections {
 		},
 	)
 	if err != nil {
-		log.Fatal(err)
+		return cols, err
 	}
-	fmt.Println(indexName)
-	return cols
+	return cols, nil
 }
 
 func main() {
@@ -78,25 +87,43 @@ func main() {
 		httpRetryCount = fs.Int("http-retry-count", 3, "Number maximum retries of http requests")
 		mongoURI       = fs.String("mongo-uri", "mongodb://localhost:27018", "Number of results per request")
 		dbName         = fs.String("db-name", "AQ_DB", "Name of used mongo db")
+		schedDuration  = fs.Uint64("scheduler-seconds", 3600, "Name of used mongo db")
 	)
-	fmt.Println("Starting")
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = level.NewFilter(logger, level.AllowInfo())
+	logger = log.With(logger, "TS:", log.DefaultTimestamp, "caller", log.DefaultCaller)
+
+	logger.Log("info", "Starting service")
 	retryClient := retryablehttp.NewClient()
 	retryClient.RetryMax = *httpRetryCount
 	retryClient.RetryWaitMin = 5 * time.Second
+	retryClient.Logger = logger.Log()
 	httpClient := retryClient.StandardClient()
-	cols := initCollections(*mongoURI, *dbName)
+	cols, err := initCollections(*mongoURI, *dbName)
+	if err != nil {
+		logger.Log("err", "Error initializing mongo collections")
+		logger.Log("err", err)
+		os.Exit(1)
+	}
 
-	// measurementsURL := fmt.Sprintf("%s/v1/latest?limit=%d&page=", *aqAPI, *batchSize)
+	measurementsURL := fmt.Sprintf("%s/v1/latest?limit=%d&page=", *aqAPI, *batchSize)
 	citiesURL := fmt.Sprintf("%s/v1/cities?limit=%d&page=", *aqAPI, *batchSize)
-	// countriesURL := fmt.Sprintf("%s/v1/countries?limit=%d&page=", *aqAPI, *batchSize)
+	countriesURL := fmt.Sprintf("%s/v1/countries?limit=%d&page=", *aqAPI, *batchSize)
 
 	dataProcessor := dataprocessor.NewDataProcessor(httpClient, *batchSize)
-	dataProcessor.ProcessData(citiesURL, cols.citiesCol.collection, dataProcessor.ProcessCities)
-	// dataProcessor.ProcessData(countriesURL, cols.countriesCol.collection, dataProcessor.ProcessCountries)
-	// dataProcessor.ProcessData(measurementsURL, cols.measurementCol.collection, dataProcessor.ProcessMeasurements)
-	// total := dataProcessor.ProcessMeasurements(fmt.Sprintf("%s%d", measurementsURL, page), cols.measurementCol.collection)
-	// for i := *batchSize; i <= total; i += *batchSize {
-	// 	page++
-	// 	dataProcessor.ProcessMeasurements(fmt.Sprintf("%s%d", measurementsURL, page), cols.measurementCol.collection)
-	// }
+	dataParams := make([]dataProcessParams, 0)
+	dataParams = append(dataParams, dataProcessParams{citiesURL, cols.citiesCol, dataProcessor.ProcessCities})
+	dataParams = append(dataParams, dataProcessParams{countriesURL, cols.countriesCol, dataProcessor.ProcessCountries})
+	dataParams = append(dataParams, dataProcessParams{measurementsURL, cols.measurementCol, dataProcessor.ProcessMeasurements})
+	for true {
+		gocron.Every(*schedDuration).Seconds().From(gocron.NextTick()).Do(processAllData, dataProcessor, dataParams)
+		<-gocron.Start()
+	}
+}
+
+func processAllData(d dataprocessor.DataProcessor, dataParams []dataProcessParams) {
+	for _, data := range dataParams {
+		logger.Log("info", fmt.Sprintf("Processing data for %s", data.url))
+		d.ProcessData(data.url, data.col.col, data.callBackFunc)
+	}
 }
--- pkg/dataprocessor/dataProcessor.go
+++ pkg/dataprocessor/dataProcessor.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
@@ -55,18 +54,17 @@ type dataProcessor struct {
 	batchSize  int
 }
 
+// DataAccessInterface that consists of all used mongo function.
 type DataAccessInterface interface {
 	BulkWrite(ctx context.Context, models []mongo.WriteModel, opts ...*options.BulkWriteOptions) (*mongo.BulkWriteResult, error)
 }
 
-//
+// DataProcessor interface for methods
 type DataProcessor interface {
-	// GetMeasurementResults(url string) ([]interface{}, int)
-	// UpsertMeasurements(colletion *mongo.Collection, resultsSlice []interface{})
-	ProcessMeasurements(url string, colletion *mongo.Collection) int
-	ProcessCities(url string, collection *mongo.Collection) int
-	ProcessCountries(url string, collection *mongo.Collection) int
-	ProcessData(url string, collection *mongo.Collection, dataProcessFunc func(url string, collection *mongo.Collection) int)
+	ProcessMeasurements(url string, colletion *mongo.Collection) (int, error)
+	ProcessCities(url string, collection *mongo.Collection) (int, error)
+	ProcessCountries(url string, collection *mongo.Collection) (int, error)
+	ProcessData(url string, collection *mongo.Collection, dataProcessFunc func(url string, collection *mongo.Collection) (int, error)) error
 }
 
 // NewDataProcessor creates a dataProcessor.
@@ -74,45 +72,58 @@ func NewDataProcessor(httpClient *http.Client, batchSize int) DataProcessor {
 	return dataProcessor{httpClient, batchSize}
 }
 
-func (d dataProcessor) ProcessMeasurements(url string, collection *mongo.Collection) int {
-	resultsSlice, total := d.getResults(url)
+func (d dataProcessor) ProcessMeasurements(url string, collection *mongo.Collection) (int, error) {
+	resultsSlice, total, err := d.getResults(url)
+	if err != nil {
+		return 0, err
+	}
 	var locResult locationResult
-	d.upsertCollection(collection, resultsSlice, &locResult, &locResult.Location, "location")
-	return total
+	err = d.upsertCollection(collection, resultsSlice, &locResult, &locResult.Location, "location")
+	return total, err
 }
 
-func (d dataProcessor) ProcessCities(url string, collection *mongo.Collection) int {
-	resultsSlice, total := d.getResults(url)
+func (d dataProcessor) ProcessCities(url string, collection *mongo.Collection) (int, error) {
+	resultsSlice, total, err := d.getResults(url)
+	if err != nil {
+		return 0, err
+	}
 	var cityRes cityResult
-	d.upsertCollection(collection, resultsSlice, &cityRes, &cityRes.Name, "name")
-	return total
+	err = d.upsertCollection(collection, resultsSlice, &cityRes, &cityRes.Name, "name")
+	return total, nil
 }
 
-func (d dataProcessor) ProcessCountries(url string, collection *mongo.Collection) int {
-	resultsSlice, total := d.getResults(url)
+func (d dataProcessor) ProcessCountries(url string, collection *mongo.Collection) (int, error) {
+	resultsSlice, total, err := d.getResults(url)
+	if err != nil {
+		return 0, err
+	}
 	var countryRes countryResult
-	d.upsertCollection(collection, resultsSlice, &countryRes, &countryRes.Code, "code")
-	return total
+	err = d.upsertCollection(collection, resultsSlice, &countryRes, &countryRes.Code, "code")
+	return total, nil
 }
 
-func (d dataProcessor) ProcessData(url string, collection *mongo.Collection, dataProcessFunc func(url string, collection *mongo.Collection) int) {
+func (d dataProcessor) ProcessData(url string, collection *mongo.Collection, dataProcessFunc func(url string, collection *mongo.Collection) (int, error)) error {
 	page := 1
-	total := dataProcessFunc(fmt.Sprintf("%s%d", url, page), collection)
+	total, err := dataProcessFunc(fmt.Sprintf("%s%d", url, page), collection)
+	if err != nil {
+		return fmt.Errorf("error processing data for url %s: %w", url, err)
+	}
 	for i := d.batchSize; i <= total; i += d.batchSize {
 		page++
 		dataProcessFunc(fmt.Sprintf("%s%d", url, page), collection)
 	}
+	return nil
 }
 
-func (d dataProcessor) getResults(url string) ([]interface{}, int) {
+func (d dataProcessor) getResults(url string) ([]interface{}, int, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatal("failed to creating a request: %w", err)
+		return nil, 0, fmt.Errorf("failed to creating a request: %w", err)
 	}
 
 	resp, err := d.httpClient.Do(req)
 	if err != nil {
-		log.Fatal((err))
+		return nil, 0, fmt.Errorf("failed to send request: %w", err)
 	}
 
 	defer resp.Body.Close()
@@ -122,54 +133,59 @@ func (d dataProcessor) getResults(url string) ([]interface{}, int) {
 
 	results, exists := result["results"]
 	if !exists {
-		log.Fatal("no results")
+		return nil, 0, fmt.Errorf("no results object present")
 	}
 
 	resultsArray, ok := results.([]interface{})
 	if !ok {
-		log.Fatal("results is not of type ")
+		return nil, 0, fmt.Errorf("could not pars results array")
 	}
 
 	meta, exists := result["meta"]
 	if !exists {
-		log.Fatal("no meta data")
+		return resultsArray, 0, fmt.Errorf("no meta data available")
 	}
 	metaMap, ok := meta.(map[string]interface{})
 	if !ok {
-		log.Fatal("results is not of type ")
+		return resultsArray, 0, fmt.Errorf("could not pars meta object")
 	}
 	total, exists := metaMap["found"].(float64)
 	if !exists {
-		fmt.Println("No valid meta data found")
+		return resultsArray, 0, fmt.Errorf("No valid meta data found")
 	}
-	return resultsArray, int(total)
+	return resultsArray, int(total), nil
 }
 
-func (d dataProcessor) upsertCollection(collection DataAccessInterface, resultsSlice []interface{}, r interface{}, filter *string, filterName string) {
+func (d dataProcessor) upsertCollection(
+	collection DataAccessInterface,
+	resultsSlice []interface{},
+	r interface{},
+	filter *string,
+	filterName string) error {
 	var operations []mongo.WriteModel
 
 	for _, result := range resultsSlice {
 		resultJSON, err := json.Marshal(result)
 		if err != nil {
-			log.Fatal(err)
+			return fmt.Errorf("error converting json: %w", err)
 		}
 		json.Unmarshal([]byte(resultJSON), &r)
 
 		mongoOperation := mongo.NewReplaceOneModel()
-		fmt.Printf("%#v \n", *filter)
 		resultCopy, err := deepCopy(r)
 		if err != nil {
-			log.Fatal(err)
+			return fmt.Errorf("error copying json: %w", err)
 		}
-		mongoOperation.SetFilter(bson.M{"name": *filter})
+		mongoOperation.SetFilter(bson.M{filterName: *filter})
 		mongoOperation.SetReplacement(resultCopy)
 		mongoOperation.SetUpsert(true)
 		operations = append(operations, mongoOperation)
 	}
 	err := bulkUpdateResult(collection, operations)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error updating collection: %w", err)
 	}
+	return nil
 }
 
 func bulkUpdateResult(collection DataAccessInterface, operations []mongo.WriteModel) error {
